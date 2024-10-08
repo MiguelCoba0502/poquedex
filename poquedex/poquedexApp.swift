06f0f278a910d920e904e9913233b429c71744ec
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-10-07 21:26:10 -0600

    Initial Commit

---
+++ poquedex/poquedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  poquedexApp.swift
+//  poquedex
+//
+//  Created by Eduardo Coba on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct poquedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
